252ecf3a377f0972068c567bbbd9adaf5b9e007c
Angela Martin <angela@angelamartin.info>
2021-01-28 12:35:24 -0500

    first commit

---
+++ src/dataStructures/Main.java
@@ -0,0 +1,47 @@
+package dataStructures;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+//        String[] my_array = {"Aaron","Joan","Layla"};
+//        System.out.printf("At index 1 is: %s\n", my_array[1]);
+//        // returns "Joan"
+//        int size = my_array.length;
+//        System.out.println(size);
+//          // returns the length or size of the array (3)
+//        
+//        //Enhanced for loop on an array
+//        for(String elem : my_array){
+//        	System.out.printf("name is: %s\n", elem);
+//        }
+//        
+//     ArrayList <Integer> myNumberList = new ArrayList<>(); 
+		
+//		ArrayList <String> nameList = new ArrayList<>();  
+//		nameList.add("John");
+//		nameList.add("Heather");
+//		nameList.add("Joe");
+// //      System.out.println(myNameList.size());
+//		nameList.get(1);
+//       //returns "Heather"
+//   
+//		
+//		//enhanced for loop
+//		for(String name : myNameList) {
+//			System.out.printf(%s\n", name);
+		
+		String[] foods = {"pop tarts", "chinese food", "salad", "steak"};
+		String allFoods = Arrays.toString(foods);
+		System.out.println(allFoods);
+		
+		}  
+
+}
+
+
+     
